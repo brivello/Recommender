16d938ab9b383cae656e4fa23d561dd17f8ae43f
Ben Rivello <[tub62574@temple.edu]>
2019-03-03 20:09:55 -0500

    added loop and timer to text input

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
+
+var recursiveAsyncReadLine = function () {
+  rl.question("Who would you like to receive recommendations for? ", function(answer) {
+  	if (answer == 'exit') {
+      return rl.close();
+  	}
+
+  	let startTime = new Date();
+	let results = getTeammateFromSearchText(answer);
+	if (results != null) {
+		let recommendations = JSON.stringify(recommend(results.id), null, '   ')
+		endTime = new Date();
+  		var timeDiff = endTime - startTime;
+	  	console.log("Restaurant recommendations for " + results.name + ":\n" + recommendations + "\n" + "Completed in: " + timeDiff + "ms");
+	} else {
+	  	console.log("Sorry, I did not recognize that name\n");
+	}
+	recursiveAsyncReadLine();
+ });
+};
+
+recursiveAsyncReadLine(); 
+
+
 rl.question("Who would you like to receive recommendations for? ", function(answer) {
 	if (answer == "exit") {
 	  	rl.close();
